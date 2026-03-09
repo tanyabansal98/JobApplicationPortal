478ffda3decb4e03f482f838f248576cf9392e0e
Tanya Bansal <254046972+tanyabansal98@users.noreply.github.com>
2026-03-09 09:57:46 -0400

    Updating student pipeline

--- src/main/java/com/job/portal/repository/StudentProfileRepository.java
+++
@@ -1,6 +0,0 @@
-package com.job.portal.repository;
-
-import com.job.portal.model.StudentProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentProfileRepository extends JpaRepository<StudentProfile, Long> {}
