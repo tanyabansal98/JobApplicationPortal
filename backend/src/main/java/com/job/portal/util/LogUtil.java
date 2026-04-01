1619ccf4406e62f9665494211526917be855d215
Tanya Bansal <254046972+tanyabansal98@users.noreply.github.com>
2026-03-31 23:52:55 -0400

    add logging utility for standardized tracing

---
+++ backend/src/main/java/com/job/portal/util/LogUtil.java
@@ -0,0 +1,11 @@
+package com.job.portal.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LogUtil {
+
+    public static Logger getLogger(Class<?> clazz) {
+        return LoggerFactory.getLogger(clazz);
+    }
+}
